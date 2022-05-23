da78e7a949b46ada8558bdb2211fa415d79216b6
LaneSawyerT <Lanefair@gmail.com>
2022-05-23 13:36:42 +0000

    deploy to heroky finale push

--- profiles/urls.py
+++ profiles/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.profile, name='profile'),
     path('add_friend/', views.add_friend, name='add_friend'),
+    path('send_message/', views.send_message, name='send_message'),
     path('accept_friend/', views.accept_friend, name='accept_friend'),
     path('delete_friend/', views.delete_friend, name='delete_friend'),
 ]
